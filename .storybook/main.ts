4b2da6a33fdaca5b3694e28e5635d5417e05c50a
Teradad41 <sitiang120@gmail.com>
2025-05-23 23:20:24 +0900

    今度こそ！

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,30 @@
 import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
-    "storycap"
-  ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
-  },
-  "staticDirs": [
-    "../public"
-  ]
+	stories: [
+		"../stories/**/*.mdx",
+		"../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+	],
+	addons: [
+		"@storybook/addon-essentials",
+		"@storybook/addon-onboarding",
+		"@chromatic-com/storybook",
+		"@storybook/experimental-addon-test",
+		"storycap",
+	],
+	framework: {
+		name: "@storybook/experimental-nextjs-vite",
+		options: {},
+	},
+	staticDirs: ["../public"],
+	viteFinal: async (config) => {
+		return {
+			...config,
+			optimizeDeps: {
+				...config.optimizeDeps,
+				include: [...(config.optimizeDeps?.include ?? []), "styled-jsx/style"],
+			},
+		};
+	},
 };
 export default config;
